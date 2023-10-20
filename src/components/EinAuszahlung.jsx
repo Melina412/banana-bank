753ff742354c46c79600dc905b581865e1529040
Melina Schild <melina.schild@icloud.com>
2023-10-20 15:20:51 +0200

    alert & saveSaldo

--- src/components/EinAuszahlung.jsx
+++ src/components/EinAuszahlung.jsx
@@ -14,13 +14,46 @@ const EinAuszahlung = () => {
     setBetrag(Number(event.target.value));
   };
 
+  //   Speichern des aktuellen Saldos um bei window.alert() das löschen des Wertes zu verhindern
+  const [savedSaldo, setSavedSaldo] = useState(saldo);
+
+  // value von saldo wird als savedSaldo gespeichert, siehe f checkSaldo()
+  const getSavedSaldo = () => {
+    setSavedSaldo(saldo);
+    console.log("setSavedSaldo -> saldo:", saldo);
+  };
+
+  const alert = () => {
+    window.alert("Du kannst nicht mehr abbuchen");
+  };
+
+  //   logs zeigen in der Konsole genau an was beim Auszahlen passiert
+  const checkSaldo = () => {
+    console.log("'Auzahlen' button wurde geklickt");
+    if (saldo - betrag > 0) {
+      console.log("-> neuer Saldo > 0, Auszahlung erfolgt");
+      console.log("-> savedSaldo bleibt unverändert");
+      setSaldo(saldo - betrag);
+    } else {
+      console.log("-> neuer Saldo < 0, Auszahlung nicht möglich");
+      console.log("-> alert() wird ausgelöst");
+      alert();
+
+      console.log("-> getSavedSaldo() wird ausgelöst");
+      getSavedSaldo();
+    }
+  };
+
   console.log({ betrag });
   console.log({ saldo });
+  console.log({ savedSaldo });
+
   return (
     <section>
       <div className="konto">
         <h2>Mein Girokonto</h2>
-        <div className="saldo">{saldo}</div> {/* Saldo wird aktualisiert */}
+        <div className="saldo">{saldo + betrag > 0 ? saldo : savedSaldo}</div>
+        {/* Saldo wird aktualisiert */}
         <input
           type="number"
           id="input"
@@ -30,16 +63,15 @@ const EinAuszahlung = () => {
           onChange={getBetrag}
         />
         <div>
-          {/* Beim click auf Buttons wird Saldo neu berechnet*/}
           <button
             className="einzahlen"
-            onClick={() => setSaldo(saldo + betrag)}
+            onClick={() => setSaldo(saldo + betrag)} // einfache Neuberechnung des Saldos
           >
             Einzahlen
           </button>
           <button
             className="auszahlen"
-            onClick={() => setSaldo(saldo - betrag)}
+            onClick={checkSaldo} // Funktion wird ausgelöst
           >
             Auszahlen
           </button>
